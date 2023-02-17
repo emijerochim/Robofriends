de5ad9de89078372a929c2ce07945cb57c0d6ce5
emijerochim <emijerochim@gmail.com>
2023-02-17 05:32:33 -0300

    index.html debug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./containers/App";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
-ReactDOM.render(
-	<App/>,
-	document.getElementById('root')
-);
+//createRoot
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 reportWebVitals();
